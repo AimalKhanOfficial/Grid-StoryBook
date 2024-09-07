9909e9b3c61e03e2e3cb629e02d9cacc5ea62d0b
AimalKhanOfficial <Aimalkhanofficial@gmail.com>
2024-09-07 16:12:13 -0400

    adding a header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import { Tile } from "./components/Tile"
+import { Footer } from "./components/Footer";
+import { Tile } from "./components/Tile";
 
 function App() {
   return (
     <>
-      <div className="flex justify-around">
-        <Tile title={'Revenue'} />
-        <Tile title={'Payments'} />
-        <Tile title={'Refunds'} />
-        <Tile title={'Payouts'} />
+      <div className="flex flex-col h-screen">
+        <div>
+          <p>Header</p>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+          <Tile title={'Revenue'} />
+          <Tile title={'Payments'} />
+          <Tile title={'Refunds'} />
+          <Tile title={'Payouts'} />
+        </div>
+        <div className="mt-auto">
+          <Footer />
+        </div>
       </div>
     </>
   )
---
+++ src/components/Footer.jsx
@@ -0,0 +1,38 @@
+export function Footer() {
+    return (
+        <div className="p-[10px] border-t-[1px] border-black">
+            <div className="grid grid-cols-2 lg:grid-cols-5 mx-auto w-[80%]">
+                <div className="flex flex-col mt-[5px] lg:mt-[0px]">
+                    <p>Option 1</p>
+                    <p>Option 2</p>
+                    <p>Option 3</p>
+                    <p>Option 4</p>
+                </div>
+                <div className="flex flex-col mt-[5px] lg:mt-[0px]">
+                    <p>Option 1</p>
+                    <p>Option 2</p>
+                    <p>Option 3</p>
+                    <p>Option 4</p>
+                </div>
+                <div className="flex flex-col mt-[5px] lg:mt-[0px]">
+                    <p>Option 1</p>
+                    <p>Option 2</p>
+                    <p>Option 3</p>
+                    <p>Option 4</p>
+                </div>
+                <div className="flex flex-col mt-[5px] lg:mt-[0px]">
+                    <p>Option 1</p>
+                    <p>Option 2</p>
+                    <p>Option 3</p>
+                    <p>Option 4</p>
+                </div>
+                <div className="flex flex-col mt-[5px] lg:mt-[0px]">
+                    <p>Option 1</p>
+                    <p>Option 2</p>
+                    <p>Option 3</p>
+                    <p>Option 4</p>
+                </div>
+            </div>
+        </div>
+    );
+}
