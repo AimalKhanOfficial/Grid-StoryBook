792e919e998c284a01b01dcc0fd7b77242f0357e
AimalKhanOfficial <Aimalkhanofficial@gmail.com>
2024-09-07 16:24:22 -0400

    adding storybook changes for Tile Component

---
+++ src/stories/Tile.stories.js
@@ -0,0 +1,20 @@
+import { Tile } from "../components/Tile";
+ 
+//👇 This default export determines where your story goes in the story list
+const meta = {
+  component: Tile
+};
+ 
+export default meta;
+ 
+export const Payments = {
+  args: {
+    title: 'Payments'
+  },
+};
+
+export const Revenue = {
+    args: {
+      title: 'Revenue'
+    },
+  };
